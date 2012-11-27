eebb9b1eec2bf16aaedc9bbf49359c9e4bad1a88
koduki <pascalm3@gmail.com>
2012-11-27 12:01:02 +0900

    modify RemoteSlideTest support betamax

--- src/test/java/cn/orz/pascal/ssv/model/BetamaxTest.java
+++ src/test/java/cn/orz/pascal/ssv/model/BetamaxTest.java
@@ -13,19 +13,42 @@ import co.freeside.betamax.Recorder;
 import org.junit.Rule;
 import org.junit.Test;
 
+import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
 
 public class BetamaxTest {
-    @Rule public Recorder recorder = new Recorder();
+    @Rule
+    public Recorder recorder = new Recorder();
 
-    @Betamax(tape="my tape")
+@Betamax(tape = "my tape")
+@Test
+public void testSimpleHttp1() throws Exception {
+    // URL接続
+    HttpURLConnection con = (HttpURLConnection) new URL("http://www.google.com/").openConnection();
+    con.setRequestMethod("GET");
+    BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+    // HTMLソースの表示
+    String str;
+    while ((str = br.readLine()) != null) {
+        System.out.println(str);
+    }
+
+    // URL切断
+    br.close();
+    con.disconnect();
+}
+
+    @Betamax(tape = "my tape2")
     @Test
-    public void testMethodThatAccessesExternalWebService()throws Exception{
+    public void testSimpleHttps1() throws Exception {
         // URL接続
-        HttpURLConnection con = (HttpURLConnection)new URL("http://www.google.com/").openConnection();
+        HttpsURLConnection con = (HttpsURLConnection) new URL("https://www.google.co.jp/").openConnection();
         con.setRequestMethod("GET");
         BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
 
